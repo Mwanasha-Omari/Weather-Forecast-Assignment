4332851b42691d6b6b4cc61cf8825593e1d40b9c
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-06-28 19:51:14 +0300

    worked on the weather forecast assignment

---
+++ src/Weather/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+const Weather = () => {
+    return(
+            <div className="search">
+                <div>
+            <h2>Ali</h2>
+            </div>
+            <div>
+            <input type="text" placeholder="Search place"/>
+            </div>
+            <button>Search</button>
+            </div>
+        
+    )
+}
+export default Weather;
